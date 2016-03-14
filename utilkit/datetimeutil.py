3f1844a65245d2c1fec50a66a1b4a214e8eaf457
Michiel Scholten <michiel@diginaut.net>
2016-03-14 16:07:19 +0100

    Text fix in comment

--- utilkit/datetimeutil.py
+++ utilkit/datetimeutil.py
@@ -17,7 +17,7 @@ def unix_to_python(timestamp):
     """
     Convert unix timestamp to python datetime
     """
-    # Not sure how correct this is to do here, but return 'null' if the timestamp from Pocket is 0
+    # Not sure how correct this is to do here, but return 'null' if the timestamp provided is 0
     if int(timestamp) == 0:
         return None
     else:
