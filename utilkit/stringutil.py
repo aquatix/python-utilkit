9b4e7a06932d6ed6a5a9032619fa433629187d69
Michiel Scholten <michiel@diginaut.net>
2016-05-09 14:12:55 +0200

    Disable error-checking that assumes Python 3 for these Python 2 helpers, landscape.io style

--- utilkit/stringutil.py
+++ utilkit/stringutil.py
@@ -5,11 +5,11 @@ String/unicode helper functions
 def safe_unicode(obj, *args):
     """ return the unicode representation of obj """
     try:
-        return unicode(obj, *args)  # pylint:disable=undefined-variable
+        return unicode(obj, *args)  # noqa for undefined-variable
     except UnicodeDecodeError:
         # obj is byte string
         ascii_text = str(obj).encode('string_escape')
-        return unicode(ascii_text)  # pylint:disable=undefined-variable
+        return unicode(ascii_text)  # noqa for undefined-variable
 
 def safe_str(obj):
     """ return the byte string representation of obj """
@@ -17,4 +17,4 @@ def safe_str(obj):
         return str(obj)
     except UnicodeEncodeError:
         # obj is unicode
-        return unicode(obj).encode('unicode_escape')  # pylint:disable=undefined-variable
+        return unicode(obj).encode('unicode_escape')  # noqa for undefined-variable
